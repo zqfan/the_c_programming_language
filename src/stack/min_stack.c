c65c74f4fe04efe9c05bdce8381a3b6017381cd4
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-05-16 20:59:55 +0800

    change description to English

---
+++ src/stack/min_stack.c
@@ -0,0 +1,9 @@
+/*
+design a stack with method min, the cost of stack_min, stack_push and
+stack_pop should be O(1)
+
+this is implemented in creacking-the-coding-interview 4th edition
+see: solution0302
+https://github.com/zqfan/crackingTheCodingInterview/blob/master/\
+crackingTheCodingInterview/cha03/3.2.cpp
+*/
--- src/tree/bst2dll.c
+++ src/tree/bst2dll.c
@@ -1,22 +1,15 @@
 /*
-把二元查找树转变成排序的双向链表
-输入一棵二元查找树，将该二元查找树转换成一个排序的双向链表。
-要求不能创建任何新的结点，只调整指针的指向。
+convert binary search tree to double linked list, you cannot create
+any new node, just can adjust pointer.
+
+sample input bst:
    10
   / /
   6  14
  / / / /
 4  8 12 16
- 转换成双向链表
+sample output dll:
 4=6=8=10=12=14=16。
-
- 首先我们定义的二元查找树 节点的数据结构如下：
- struct BSTreeNode
-{
-  int m_nValue; // value of node
-  BSTreeNode *m_pLeft; // left child of node
-  BSTreeNode *m_pRight; // right child of node
-};
 */
 #include<stdlib.h>
 #include<stdio.h>
