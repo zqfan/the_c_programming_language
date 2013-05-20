e75a58ae9b3895b732c473d596cba0d8c924bd6c
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-05-20 15:09:10 +0800

    exercise 2-1

---
+++ src/the_c_programming_language_2/chap02/01.c
@@ -0,0 +1,39 @@
+/* Write a program to determine the ranges of char, short, int, and
+   long variables, both signed and unsigned, by printing appropriate
+   values from standard headers and by direct computation. Harder if
+   you compute them: determine the ranges of the various
+   floating-point types.
+*/
+#include<stdio.h>
+#include<limits.h>
+#include<float.h>
+
+/* /usr/include/limits.h */
+/* /usr/lib/gcc/i686-linux-gnu/4.6/include/float.h */
+int main() {
+  /* range of char */
+  printf("signed char min: %d, max: %d\n", SCHAR_MIN, SCHAR_MAX);
+  printf("unsigned char min: %d, max: %d\n", 0, UCHAR_MAX);
+
+  /* range of short */
+  printf("short min: %d, max: %d\n", SHRT_MIN, SHRT_MAX);
+  printf("unsigned short min: %d, max: %d\n", 0, USHRT_MAX);
+
+  /* range of int */
+  printf("int min: %d, max: %d\n", INT_MIN, INT_MAX);
+  printf("unsigned int min: %u, max: %u\n", 0, UINT_MAX);
+
+  /* range of long */
+  printf("long int min: %ld, max: %ld\n", LONG_MIN, LONG_MAX);
+  printf("unsigned long int min: %lu, max: %lu\n", 0ul, ULONG_MAX);
+
+  /* range of float
+     here you must use %e for print format, or the min will be 0(wrong
+     answer) and the max will print very long(not pretty)
+  */
+  printf("float min: %e, max: %e\n", FLT_MIN, FLT_MAX);
+  printf("doule float min: %le, max: %le\n", DBL_MIN, DBL_MAX);
+  printf("long double float min: %Le, max: %Le\n", LDBL_MIN, LDBL_MAX);
+
+  return 0;
+}
