3957baf9d97d57843a36151372f6fc68da8f556d
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-05-22 23:31:37 +0800

    exercise 1-1

---
+++ src/the_c_programming_language_2/chap01/0101.c
@@ -0,0 +1,10 @@
+/* Run the ``hello, world'' program on your system. Experiment with
+   leaving out parts of the program, to see what error messages you
+   get.
+*/
+#include <stdio.h>
+
+int main() {
+  printf("Hello, hell.\n");
+  return 0;
+}
