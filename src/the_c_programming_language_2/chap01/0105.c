60edca09ad30182195aa195d649b707a4908b294
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-05-22 23:53:03 +0800

    exercise 1-5

---
+++ src/the_c_programming_language_2/chap01/0105.c
@@ -0,0 +1,21 @@
+/* Modify the temperature conversion program to print the table in
+   reverse order, that is, from 300 degrees to 0. */
+
+#include <stdio.h>
+
+/* print Fahrenheit-Celsius table
+   for fahr = 0, 20, ..., 300; floating-point version */
+main() {
+  float fahr, celsius;
+  float lower, upper, step;
+  lower = 0;     /* lower limit of temperatuire scale */
+  upper = 300;   /* upper limit */
+  step = -20;     /* step size */
+  fahr = upper;
+  printf("%3s %6s\n", "Fah", "Cels");
+  while (fahr >= lower) {
+    celsius = (5.0/9.0) * (fahr-32.0);
+    printf("%3.0f %6.1f\n", fahr, celsius);
+    fahr += step;
+  }
+}
