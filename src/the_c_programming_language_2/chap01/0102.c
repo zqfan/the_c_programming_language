93b040adf8fa03137afd1a4b022922decdfeb2db
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-05-22 23:37:19 +0800

    exercise 1-2

---
+++ src/the_c_programming_language_2/chap01/0102.c
@@ -0,0 +1,9 @@
+/* Experiment to find out what happens when prints's argument string
+   contains \c, where c is some character not listed above.
+*/
+#include <stdio.h>
+
+int main() {
+  printf("\r\v");
+  return 0;
+}
