7281bdc36415eacbe469bf77743f1eaba7cfe61b
Zhiqiang Fan <aji.zqfan@gmail.com>
2013-05-23 00:59:03 +0800

    exercise 1-12

---
+++ src/the_c_programming_language_2/chap01/0112.c
@@ -0,0 +1,19 @@
+/* Write a program that prints its input one word per line. */
+#include <stdio.h>
+
+#define IN 1
+#define OUT 0
+
+void main() {
+  int c, state;
+  state = OUT;
+  while ((c = getchar()) != EOF) {
+    if (c != ' ' && c != '\t' && c != '\n') {
+      state = IN;
+      putchar(c);
+    } else if (state == IN) {
+      putchar('\n');
+      state = OUT;
+    }
+  }
+}
